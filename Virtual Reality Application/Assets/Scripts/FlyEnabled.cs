3991de8424b3c9f4b5a3cf57cc5bf61bdd9a9f3e
Belal Dirani <b_dirani@hotmail.com>
2017-08-15 21:13:09 +1000

    changes

--- Virtual Reality Application/Assets/Scripts/FlyEnabled.cs
+++ Virtual Reality Application/Assets/Scripts/FlyEnabled.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript1 : MonoBehaviour {
+public class FlyEnabled : MonoBehaviour {
 
     public GameObject cameraFly;
     // Use this for initialization
